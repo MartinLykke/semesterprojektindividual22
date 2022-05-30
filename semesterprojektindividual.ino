23a9464ec517a41c10f0f77c543bb1290a9b9ff5
martinlykke <martin@wirksomhed.dk>
2022-05-30 10:58:10 +0200

    Init commit

---
+++ semesterprojektindividual.ino
@@ -0,0 +1,64 @@
+#include "DHT.h"
+#define DHTPIN 2     // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT11   // DHT 11
+DHT dht(DHTPIN, DHTTYPE);
+
+#define SensorPin A1
+float sensorValue = 0; 
+
+// Ultrasonic sensor
+#define echoPin 2 // attach pin D2 Arduino to pin Echo of HC-SR04
+#define trigPin 3 //attach pin D3 Arduino to pin Trig of HC-SR04
+// defines variables
+long duration; // variable for the duration of sound wave travel
+int distance; // variable for the distance measurement
+
+void setup()
+{
+  Serial.begin(9600); // open serial port, set the baud rate to 9600 bps
+
+  dht.begin();
+}
+void loop()
+{
+      int val;
+      val=analogRead(0);   //connect grayscale sensor to Analog 0
+       Serial.print("Light: ");
+      Serial.println(val,DEC);//print the value to serial
+
+        float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print(F(" Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("C "));
+  Serial.print(f);
+  Serial.print(F("F  Heat index: "));
+  Serial.print(hic);
+  Serial.print(F("C "));
+  Serial.print(hif);
+  Serial.println(F("F"));
+
+    float moisture = analogRead(SensorPin);
+
+ Serial.print("Moisture: ");
+ Serial.println(moisture);
+   //Serial.println(String(moisture, 2) + String("% Moist"));
+      delay(1000);
+}
