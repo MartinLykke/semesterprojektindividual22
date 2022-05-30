344e426e92d97c274d4cd4475c6e6b26638f71e4
martinlykke <martin@wirksomhed.dk>
2022-05-30 12:46:13 +0200

    Added support for two more sensors

--- semesterprojektindividual.ino
+++ semesterprojektindividual.ino
@@ -7,26 +7,31 @@ DHT dht(DHTPIN, DHTTYPE);
 float sensorValue = 0; 
 
 // Ultrasonic sensor
-#define echoPin 2 // attach pin D2 Arduino to pin Echo of HC-SR04
-#define trigPin 3 //attach pin D3 Arduino to pin Trig of HC-SR04
+#define echoPin 3 // attach pin D2 Arduino to pin Echo of HC-SR04
+#define trigPin 4 //attach pin D3 Arduino to pin Trig of HC-SR04
 // defines variables
 long duration; // variable for the duration of sound wave travel
 int distance; // variable for the distance measurement
 
+#define RELAY_PIN 5 // ESP32 pin 0, which connects to the IN pin of relay
+
 void setup()
 {
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
+  pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, HIGH);
   Serial.begin(9600); // open serial port, set the baud rate to 9600 bps
 
   dht.begin();
 }
 void loop()
 {
-       int val;
+
       int val;
       val=analogRead(0);   //connect grayscale sensor to Analog 0
        Serial.print("Light: ");
       Serial.println(val,DEC);//print the value to serial
-
         float h = dht.readHumidity();
   // Read temperature as Celsius (the default)
   float t = dht.readTemperature();
@@ -61,5 +66,31 @@ void loop()
  Serial.print("Moisture: ");
  Serial.println(moisture);
    //Serial.println(String(moisture, 2) + String("% Moist"));
-      delay(1000);
+
+    // Clears the trigPin condition
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+  // Displays the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  if(moisture<300 && distance<13){
+    digitalWrite(RELAY_PIN, LOW);
+    } else {
+          digitalWrite(RELAY_PIN, HIGH);
+      }
+  if(t>20 && val>600 && distance<13){
+    digitalWrite(RELAY_PIN, LOW);
+    } 
+
+      delay(2000);
 }
